478e520d0787068bd4a1ad7bf5658ce3d8b4bec7
FrederikDanielsen <124146236+FrederikDanielsen@users.noreply.github.com>
2025-01-07 14:19:04 +0100

    Exercise 3
    Exercise 3

--- Exercise3/Exercise3.ino
+++ Exercise3/Exercise3.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const uint8_t pinButton = 5;
+const uint8_t pinLED = 4;
+
+
+bool ON = false;
+bool buttonDown = false;
+bool pressed = false;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinButton, INPUT_PULLUP);
+  pinMode(pinLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+  if (!digitalRead(pinButton)) {
+    pressed = true;
+  }
+
+  if (pressed && digitalRead(pinButton)) {
+    if (ON) {
+      ON = false;
+      digitalWrite(pinLED, LOW);
+    } else {
+      ON = true;
+      digitalWrite(pinLED, HIGH);
+    }
+    pressed = false;
+    delay(5);  // To prevent button rebounce
+  }
+
 
 }
